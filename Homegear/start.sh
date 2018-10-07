cea39c756087b86c07fc2253f66159e57786dcf0
Clemens Putschli <clemens@putschli.de>
2018-10-07 09:52:26 +0200

    Update start.sh

--- Homegear/start.sh
+++ Homegear/start.sh
@@ -36,10 +36,10 @@ if ! [ -f /etc/homegear/dh1024.pem ]; then
 	openssl req -batch -new -key /etc/homegear/homegear.key -out /etc/homegear/homegear.csr
 	openssl x509 -req -in /etc/homegear/homegear.csr -signkey /etc/homegear/homegear.key -out /etc/homegear/homegear.crt
 	rm /etc/homegear/homegear.csr
-	chown homegear:homegear /etc/homegear/homegear.key
+	chown root:root /etc/homegear/homegear.key
 	chmod 400 /etc/homegear/homegear.key
 	openssl dhparam -check -text -5 -out /etc/homegear/dh1024.pem 1024
-	chown homegear:homegear /etc/homegear/dh1024.pem
+	chown root:root /etc/homegear/dh1024.pem
 	chmod 400 /etc/homegear/dh1024.pem
 fi
 
